056a934ff463a98bdd9cc26f399b0ba5c98bf599
Javed Hussain <javed.hussain@sina.pk>
2024-10-29 10:43:05 +0500

    additions

--- src/api/products.js
+++ src/api/products.js
@@ -1,7 +1,15 @@
 import axios from "axios"
 
-export function getProducts() {
+export function getProducts(page) {
+  const limit = 10;
+  const offset = (page - 1) * limit;
   return axios
-    .get("https://api.escuelajs.co/api/v1/products")
-    .then(res => res.data)
-}
+    .get(`https://api.escuelajs.co/api/v1/products?offset=${offset}&limit=${limit}`)
+    .then(res => {
+      // const hasNext = page * 2 <= parseInt(res.headers["x-total-count"])
+      return {
+        nextPage: res.data.length == 10 ? true: false,
+        previousPage: offset == 0 ? false: true,
+        products: res.data,
+      }})
+    }
--- src/components/products/Card.jsx
+++ src/components/products/Card.jsx
@@ -5,13 +5,13 @@ const Card = ({ product }) => {
   const handleAddClick = (e) => {
     e.stopPropagation(); // Prevents navigation
     // Add functionality for the Add button here (e.g., adding to cart)
-    console.log(`Added ${product.title} to cart`);
+    console.log(`Added ${product?.title} to cart`);
   };
 
   return (
     <div className='max-w-[240px] rounded-md border-2 border-gray-500 p-2'>
-      <Link to={`/products/${product.id}`} className='w-full rounded-md block'>
-        <img src={product?.images[0]} alt={`${product?.title}'s image`} />
+      <Link to={`/products/${product?.id}`} className='w-full rounded-md block'>
+        <img src={product?.images[0][0]} alt={`${product?.title}'s image`} />
         <h2>{product?.title}</h2>
       </Link>
       <div className='flex justify-between items-center mt-3'>
